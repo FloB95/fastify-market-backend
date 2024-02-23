e963ff4234195ce3b288141af09bb586120b2c7d
Florian Breuer <florian.breuer@medium.ag>
2024-02-23 10:45:58 +0100

    update total

--- src/modules/user/infrastructure/use_cases/GetUsers.ts
+++ src/modules/user/infrastructure/use_cases/GetUsers.ts
@@ -1,6 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { inject, injectable } from 'tsyringe'
-import { type User } from '../../domain/entities/User'
 import { type IGetUsersUseCase } from '../../domain/use_cases/IGetUsersUseCase'
 import { IUserRepository } from '../../domain/repositories/IUserRepository'
 
@@ -13,7 +12,7 @@ export class GetUsersUseCase implements IGetUsersUseCase {
   async execute(page: number, limit: number) {
     const offset = (page - 1) * limit
     const users = await this.userRepository.findAll(limit, offset)
-    const total = 1
+    const total = await this.userRepository.countTotal()
 
     return {
       users,
