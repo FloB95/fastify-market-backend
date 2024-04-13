1e29ad6adbdea825a7e1aeab1635b52ca82e36ff
Florian Breuer <florian.breuer@medium.ag>
2024-04-13 12:26:29 +0200

    log server start

--- src/core/infrastructure/fastify/server.ts
+++ src/core/infrastructure/fastify/server.ts
@@ -125,6 +125,8 @@ export const startServer = async () => {
 
     await server.listen(serverOptions)
 
+    console.log("Server is running under url http://%s:%s", serverOptions.host, serverOptions.port)
+
     return server
   } catch (e) {
     console.error(e)
